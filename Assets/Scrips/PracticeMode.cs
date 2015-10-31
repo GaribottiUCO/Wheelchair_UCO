72a2aa899ed9a1b23caaa23177b678f3eec8bb18
GaribottiUCO <DanielGaribotti@Daniels-MacBook-Pro.local>
2015-10-30 23:40:59 -0500

    Creating project
    Project from Qing

---
+++ Assets/Scrips/PracticeMode.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PracticeMode : MonoBehaviour {
+
+	public void ChangeToScene (string sceneToChangeTo){
+		Application.LoadLevel(sceneToChangeTo);
+	}
+}
